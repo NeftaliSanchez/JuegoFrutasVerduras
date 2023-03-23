45f38da5063eee65af47086f969c6da26cdf49ff
Neftalí Sánchez <neftali.sanchez280596@gmail.com>
2023-03-23 14:23:47 -0600

    camera

--- select_camera.py
+++ select_camera.py
@@ -7,8 +7,9 @@ import cv2
 def screenVideo():
     # PC - 0
     # Laptop - 2
-    # if you have troveled shotting your camera, then
-    # you'd need to change the number at cv2.VideoCapture(X)
+    # if you have trouble shotting your camera, then
+    # you need to change the number at cv2.VideoCapture(X)
+    # where X could be 0,1, 2
 
     cap = cv2.VideoCapture(0)
     return cap
